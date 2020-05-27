0c4c8a632346f3a2621c4d8e20f4ae380d23979a
dariathompson <daria.kambalina@gmail.com>
2020-05-27 15:58:56 +0100

    added form for creating exercise log

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 
@@ -13,12 +12,14 @@ import CreateUser from "./components/create-user.component"
 function App() {
   return (
     <Router>
-      <Navbar />
-      <br/>
-      <Route path="/" exact component={ExercisesList} />
-      <Route path="/edit/:id" component={EditExercise} />
-      <Route path="/create" component={CreateExercise} />
-      <Route path="/user" component={CreateUser} />
+        <Navbar />
+        <div className="container">
+        <br/>
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+      </div>
     </Router>
   );
 }
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -4,19 +4,21 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     return (
       <nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
-        <Link to="/" className="navbar-brand">Exercise Tracker</Link>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="navbar-item">
-              <Link to="/" className="nav-link">Exercices</Link>
-            </li>
-            <li className="navbar-item">
-              <Link to="/create" className="nav-link">Create Exercise Log</Link>
-            </li>
-            <li className="navbar-item">
-              <Link to="/user" className="nav-link">Create User</Link>
-            </li>
-          </ul>
+        <div className="container">
+          <Link to="/" className="navbar-brand">Exercise Tracker</Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link">Exercices</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/create" className="nav-link">Create Exercise Log</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/user" className="nav-link">Create User</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     )
